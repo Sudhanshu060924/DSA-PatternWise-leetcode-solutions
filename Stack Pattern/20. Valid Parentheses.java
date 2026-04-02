f8ce5e1061fc2de696e4ba48745a6a372f9b48fb
Sudhanshu060924 <sudhanshusingh0624@gmail.com>
2026-04-02 20:43:08 +0530

    Stack Pattern ques 20

---
+++ Stack Pattern/20. Valid Parentheses.java
@@ -0,0 +1,37 @@
+//https://leetcode.com/problems/valid-parentheses?listId=552y65ke&page=1
+
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+
+        Stack<Character> st = new Stack();
+        int n = s.length();
+
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[') {
+                st.push(s.charAt(i));
+            } 
+            else {
+                if (st.empty()) {
+                    return false;
+                }
+
+                if (s.charAt(i) == ')' && st.peek() == '(') {
+                    st.pop();
+                } else if (s.charAt(i) == '}' && st.peek() == '{') {
+                    st.pop();
+                } else if (s.charAt(i) == ']' && st.peek() == '[') {
+                    st.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        if (!(st.empty())) {
+            return false;
+        }
+        return true;
+
+    }
+}
