677d633a114738747d8713be8f09bef96dd45bd4
Sudhanshu060924 <sudhanshusingh0624@gmail.com>
2026-05-02 13:20:06 +0530

    2 pointer

---
+++ 2-Pointer/80. Remove Duplicates from Sorted Array II.java
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii?envType=problem-list-v2&envId=two-pointers
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+
+        if(nums.length<=2) return nums.length;
+
+        int k =2;
+
+        for(int i=2;i<nums.length;i++){
+            if(nums[k-2] != nums[i]){
+                nums[k] = nums[i];
+                k++;
+            }
+
+        }
+        return k;
+
+        
+    }
+}
