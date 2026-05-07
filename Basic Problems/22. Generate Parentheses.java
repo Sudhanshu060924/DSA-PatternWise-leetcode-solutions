177653c6960c57e71f4bfd13f9931688a0a0472a
Sudhanshu060924 <sudhanshusingh0624@gmail.com>
2026-05-07 22:21:33 +0530

    basic

---
+++ Basic Problems/22. Generate Parentheses.java
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/generate-parentheses?listId=552y65ke&page=1
+
+class Solution {
+
+    public void solve(int open, int close, int n, String s, List<String> ans) {
+
+        if(s.length() == 2 * n) {
+            ans.add(s);
+            return;
+        }
+
+        if(open < n) {
+            solve(open + 1, close, n, s + "(", ans);
+        }
+
+        if(close < open) {
+            solve(open, close + 1, n, s + ")", ans);
+        }
+    }
+
+    public List<String> generateParenthesis(int n) {
+
+        List<String> ans = new ArrayList<>();
+
+        solve(0, 0, n, "", ans);
+
+        return ans;
+    }
+}
