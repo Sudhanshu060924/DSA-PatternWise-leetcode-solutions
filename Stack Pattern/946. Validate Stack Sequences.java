c9482f47abe2c5fed23272c29c26c0a4c6fa4726
Sudhanshu060924 <sudhanshusingh0624@gmail.com>
2026-05-16 19:31:28 +0530

    Stack

---
+++ Stack Pattern/946. Validate Stack Sequences.java
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/validate-stack-sequences?listId=552y65ke&page=1
+
+import java.util.Stack;
+
+class Solution {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+
+        int j =0;
+        Stack<Integer> st = new Stack();
+        if(pushed.length != popped.length) return false;
+
+        for(int i =0; i<pushed.length;i++){
+            st.push(pushed[i]);
+
+           while (!st.isEmpty() && st.peek() == popped[j]) {
+                st.pop();
+                j++;
+            }
+        }
+
+        return st.empty();
+        
+    }
+}
