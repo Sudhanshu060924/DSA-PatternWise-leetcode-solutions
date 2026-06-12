2955b598bad032114423379c36fad571ad6581ac
Sudhanshu060924 <Sudhanshusingh0624@gmail.com>
2026-06-12 23:48:53 +0530

    58

---
+++ Basic Problems/58. Length of Last Word.java
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/length-of-last-word
+
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int len = 0;
+        int i = s.length() - 1;
+
+        // Skip trailing spaces
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
+        }
+
+        // Count characters of last word
+        while (i >= 0 && s.charAt(i) != ' ') {
+            len++;
+            i--;
+        }
+
+        return len;
+    }
+}
