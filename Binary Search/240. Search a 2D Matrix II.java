4a7107aba67838fb20dec866c924004d0d4f65ba
Sudhanshu060924 <Sudhanshusingh0624@gmail.com>
2026-07-04 18:07:31 +0530

    2d array binary search

---
+++ Binary Search/240. Search a 2D Matrix II.java
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/search-a-2d-matrix-ii
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int row = m-1;
+        int col= 0;
+        while(row>=0 && col<n){
+            int val = matrix[row][col];
+            if(val == target) {
+                return true;
+            }
+
+            if(val>target){
+                row--;
+            }
+            else{
+                col++;
+            }
+        }
+        return false;
+        
+    }
+}
