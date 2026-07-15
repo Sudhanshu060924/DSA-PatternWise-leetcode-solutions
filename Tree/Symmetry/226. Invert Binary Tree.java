30692d5705d46867b9fd4a887e997275b32de6a7
Sudhanshu060924 <Sudhanshusingh0624@gmail.com>
2026-07-15 20:04:24 +0530

    tree ques symm

---
+++ Tree/Symmetry/226. Invert Binary Tree.java
@@ -0,0 +1,38 @@
+// https://leetcode.com/problems/invert-binary-tree
+
+import javax.swing.tree.TreeNode;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+
+        return fun(root);
+        
+    }
+
+    TreeNode fun(TreeNode root){
+        if(root == null) return root;
+
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        fun(root.left);
+        fun(root.right);
+
+        return root;
+    }
+}
