7c5f42531817a8317eecb94d38c842e33d77d81e
Sudhanshu060924 <Sudhanshusingh0624@gmail.com>
2026-08-02 21:52:59 +0530

    Started DP

---
+++ Dynamic Programing/70. Climbing Stairs.java
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/climbing-stairs
+
+import java.util.HashMap;
+
+class Solution {
+    HashMap<Integer,Integer> dp = new HashMap<>();
+    public int climbStairs(int n) {
+        return fun(0,n);
+        
+    }
+    int fun(int i , int n){
+        if(i == n) return 1;
+        if(i>n) return 0;
+        if(dp.containsKey(i)){
+            return dp.get(i);
+        }
+        int a1 = fun(i+1,n);
+        int a2 = fun(i+2,n);
+        dp.put(i,a1+a2);
+        return a1+a2;
+    }
+}
